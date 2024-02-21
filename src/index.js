73b8331e6c6d3be4e44803aa8afa720c80db2eb7
viz-nu <vishnu.teja101.vt@gmail.com>
2024-02-21 13:38:38 +0530

    fix:cors

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 startCronJob();
 dbConnect();
 
-const whitelist = ["https://viznu.dev"];
+const whitelist = ["https://campusroot.com"];
 app.set("trust proxy", 1); // trust first proxy
 app.use(
 	session({
