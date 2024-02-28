f5246c0eb3e895987155a6d6d91eb4a96adfac05
viz-nu <vishnu.teja101.vt@gmail.com>
2024-02-28 12:55:46 +0530

    fix: limiting regex values

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,9 +1,9 @@
 import { subDisciplineEnum,disciplineEnum } from "./enum.js"
 export const disciplineRegexMatch = (search) => {
     const regex = new RegExp(search, "i");
-    return Object.values(disciplineEnum).filter(ele => regex.test(ele));
+    return Object.values(disciplineEnum).filter(ele => regex.test(ele)).slice(0, 5);
 }
 export const subDisciplineRegexMatch = (search) => {
     const regex = new RegExp(search, "i");
-    return Object.values(subDisciplineEnum).filter(ele => regex.test(ele));
+    return Object.values(subDisciplineEnum).filter(ele => regex.test(ele)).slice(0, 5);
 }
