80f639332827a4b26be8f7e921666049720048c3
viz-nu <vishnu.teja101.vt@gmail.com>
2024-02-29 23:45:35 +0530

    misc: testing clusters

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ import cluster from "cluster";
 import os from "os";
 import 'dotenv/config';
 import { startCronJob } from "./utils/cron.js";
-// import { rateLimit } from 'express-rate-limit'
+import indexRouter from "./routers/index.js";
+import { sendNotification } from "./utils/sendNotification.js";
+
 const app = express();
 const server = createServer(app);
 
@@ -54,10 +56,7 @@ const corsOptions = {
 	credentials: true,
 	optionsSuccessStatus: 200
 };
-app.use(compression({
-	level: 6,
-	threshold: 10 * 100
-}))
+app.use(compression({ level: 6, threshold: 10 * 100 }))
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -66,62 +65,10 @@ app.use(express.json({ type: ["application/json", "text/plain"], }));
 app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], imgSrc: ["'self'", "data:", "https://lh3.googleusercontent.com", "https://res.cloudinary.com", "https://www.4icu.org/", "https://icon-library.com/", "blob:"], }, }));
 app.use(mongoSanitize());
 app.use(morgan("tiny"));
-// app.use(rateLimit({
-// 	windowMs: 1 * 60 * 1000, // 1 minute
-// 	limit: 30, // Limit each IP to 10 requests per `window` (here, per 1 minutes)
-// 	message: 'Too many requests from this IP, please try again after 2 minutes',  
-// 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-// }))
-
-
-// const apiLimiter = rateLimit({
-// 	windowMs: 1 * 60 * 1000, // 1 minute
-// 	max: 30, // Limit each IP to 30 requests per window
-// 	message: 'Too many requests from this IP, please try again after 2 minutes',
-// 	headers: true // Include headers in the response
-//   });                                                            put this as middleware where ever it is necessary
-
-
-// // Apply rate limiter middleware to a specific route
-// app.get('/api/v1/specific-route', apiLimiter, function(req, res) {
-// 	// Your route logic here
-// 	res.send('This is the specific route with rate limiting');
-//   });
-
-//   // Other routes without rate limiting
-//   app.get('/', function(req, res) {
-// 	res.send('server up and running');
-//   });
-
-
-
-import indexRouter from "./routers/index.js";
-import { sendNotification } from "./utils/sendNotification.js";
-
-
-
 app.use("/api/v1", indexRouter);
-app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-
-const io = new Server(server, {
-	cors: {
-		origin: function (origin, callback) {
-			if (!origin || whitelist.indexOf(origin) !== -1) {
-				callback(null, true);
-			} else {
-				callback(new Error("Not allowed by CORS"));
-			}
-		},
-		credentials: true,
-	},
-}); // Initialize Socket.IO
-io.use((socket, next) => {
-	next();
-});
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
+const io = new Server(server, { cors: { origin: (origin, callback) => (!origin || whitelist.indexOf(origin) !== -1) ? callback(null, true) : callback(new Error("Not allowed by CORS")), credentials: true, }, }); // Initialize Socket.IO
+io.use((socket, next) => next());
 // Socket.IO event handlers
 io.on('connection', function (socket) {
 	console.log("new user connected");
