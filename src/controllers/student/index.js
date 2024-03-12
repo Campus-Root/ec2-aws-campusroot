2b79c7818d9d8392a9214d2746924804075fac97
viz-nu <vishnu.teja101.vt@gmail.com>
2024-03-12 10:36:00 +0530

    fix: phone

--- src/controllers/student/index.js
+++ src/controllers/student/index.js
@@ -64,7 +64,11 @@ export const profile = errorWrapper(async (req, res, next) => {
 
 export const editPhone = errorWrapper(async (req, res, next) => {
   const { phone } = req.body
-  const existingPhone = await studentModel.find({ "phone.countryCode": phone.countryCode, "phone.number": phone.number }, "phone")
+  if (!phone.countryCode || !phone.number) return next(generateAPIError(`Enter a valid number`, 400));
+  const existingPhone = await studentModel.find({  $and: [
+    { "phone.countryCode": phone.countryCode },
+    { "phone.number": phone.number }
+  ]}, "phone")
   if (existingPhone.length > 0) return next(generateAPIError(`phone number already exists, Enter a new number`, 400));
   req.user.phone = phone;
   req.user.phoneVerified = false;
