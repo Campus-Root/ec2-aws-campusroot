8ddcb8b9e8b01b48a4ae7772b7aadfe27723bc07
viz-nu <vishnu.teja101.vt@gmail.com>
2024-04-12 11:03:44 +0530

    test: freeing up space

--- src/controllers/student/index.js
+++ src/controllers/student/index.js
@@ -326,37 +326,7 @@ export const generateRecommendations = errorWrapper(async (req, res, next) => {
   }
   return res.status(200).json({ success: true, message: "Recommendations Generated", data: req.user.recommendation, AccessToken: req.AccessToken ? req.AccessToken : null });
 })
-// const { Secret_key } = config.get("STRIPE_PAYMENTS")
-// const stripe = new Stripe(`${Secret_key}`);
-// const URL = config.get("URL")
-// export const checkout = async (req,res,next) => {
-//     try {
-//         const { price, universityId, courseId } = req.body
 
-//         if (!price === NaN) return res.status(400).send("enter valid price");
-//         const session = await stripe.checkout.sessions.create({
-//             line_items: [
-//                 {
-//                     price_data: {
-//                         currency: 'inr',
-//                         product_data: { name: 'CampusRoot' },
-//                         unit_amount: price * 100,
-//                     },
-//                     quantity: 1,
-//                 },
-//             ],
-//             mode: 'payment',
-//             success_url: `${URL}/api/v1/student/success?universityIds=${universityId}&courseIds=${courseId}&userId=${req.user._id}`,
-//             cancel_url: `${URL}/api/v1/student/failed`,
-//             customer_email: req.user.email,
-//         });
-//         // generate an invoice and email it to user with session.id
-//         return res.status(200).json({ url: session.url });
-
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
 export const activity = errorWrapper(async (req, res, next) => {
   await Promise.all([
     await req.user.populate("counsellor", "numberOfStudentsAssisted linkedIn appointmentLink name email displayPicSrc"),
@@ -975,5 +945,34 @@ export const bookedSlots = errorWrapper(async (req, res, next) => {
   return res.status(200).json({ success: true, message: `booked slots`, data: meetings, AccessToken: req.AccessToken ? req.AccessToken : null });
 })
 
+// const { Secret_key } = config.get("STRIPE_PAYMENTS")
+// const stripe = new Stripe(`${Secret_key}`);
+// const URL = config.get("URL")
+// export const checkout = async (req,res,next) => {
+//     try {
+//         const { price, universityId, courseId } = req.body
 
+//         if (!price === NaN) return res.status(400).send("enter valid price");
+//         const session = await stripe.checkout.sessions.create({
+//             line_items: [
+//                 {
+//                     price_data: {
+//                         currency: 'inr',
+//                         product_data: { name: 'CampusRoot' },
+//                         unit_amount: price * 100,
+//                     },
+//                     quantity: 1,
+//                 },
+//             ],
+//             mode: 'payment',
+//             success_url: `${URL}/api/v1/student/success?universityIds=${universityId}&courseIds=${courseId}&userId=${req.user._id}`,
+//             cancel_url: `${URL}/api/v1/student/failed`,
+//             customer_email: req.user.email,
+//         });
+//         // generate an invoice and email it to user with session.id
+//         return res.status(200).json({ url: session.url });
 
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
