8a7b16ac8209f5109c44510846e22e23339826d9
viz-nu <vishnu.teja101.vt@gmail.com>
2024-04-24 00:21:54 +0530

    fix: robots.txt

--- src/index.js
+++ src/index.js
@@ -82,6 +82,7 @@ app.use(helmet.permittedCrossDomainPolicies({ permittedPolicies: 'none' }));
 app.use(mongoSanitize());
 app.use(morgan("tiny"));
 app.use("/api/v1", indexRouter);
+app.get('/robots.txt', (req, res) => { res.type('text/plain').send('User-agent: *\nDisallow: /'); });
 // app.get('/*', (req, res) => res.send("server is up and running"));
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 const io = new Server(server, { cors: { origin: (origin, callback) => (!origin || whitelist.indexOf(origin) !== -1) ? callback(null, true) : callback(new Error("Not allowed by CORS")), credentials: true, }, }); // Initialize Socket.IO
