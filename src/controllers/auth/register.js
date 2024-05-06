7c8047de5dd515ce38cd91b6313a58de96786795
viz-nu <vishnu.teja101.vt@gmail.com>
2024-05-06 14:37:20 +0530

    fix: minor bug

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -12,7 +12,7 @@ import Handlebars from "handlebars";
 import path from "path";
 import { fileURLToPath } from "url";
 import { jwtDecode } from "jwt-decode";
-
+import { cookieOptions } from "../../index.js";
 const ACCESS_SECRET = process.env.ACCESS_SECRET
 const REFRESH_SECRET = process.env.REFRESH_SECRET
 
