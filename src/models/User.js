72431d3156f6669d314822434774a994a1df3f40
viz-nu <vishnu.teja101.vt@gmail.com>
2024-05-20 16:15:57 +0530

    fix:removed default: cloudinary image

--- src/models/User.js
+++ src/models/User.js
@@ -5,7 +5,7 @@ import validator from "validator";
 const userSchema = mongoose.Schema({
   firstName: { type: String, trim: true, },
   lastName: { type: String, trim: true, },
-  displayPicSrc: { type: String, default: "https://res.cloudinary.com/dffdp7skh/image/upload/v1706879929/nvitahnrlhvmtcizpthx.png" },
+  displayPicSrc: { type: String, default: "" },
   email: { type: String, required: [true, "Please provide email"], unique: true, validate: { validator: validator.isEmail, message: "please provide valid email", }, trim: true, },
   password: { type: String, trim: true },
   socialAuth: { type: Object },
