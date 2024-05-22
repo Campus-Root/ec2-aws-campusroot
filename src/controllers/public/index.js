40d038c865add6f705f267a0f9f2fd3ea1aa006f
viz-nu <vishnu.teja101.vt@gmail.com>
2024-05-22 13:41:45 +0530

    test: courses in university

--- src/controllers/public/index.js
+++ src/controllers/public/index.js
@@ -202,7 +202,7 @@ export const uniNameRegex = errorWrapper(async (req, res, next) => {
             { name: { $regex: req.query.search, $options: "i" } },
             { code: { $regex: req.query.search, $options: "i" } }
         ],
-        courses: { $exists: true, $not: { $size: 0 } }
+        // courses: { $exists: true, $not: { $size: 0 } }
     };
     const disciplineSearchResults = disciplineRegexMatch(req.query.search)
     const subDisciplineSearchResults = subDisciplineRegexMatch(req.query.search)
