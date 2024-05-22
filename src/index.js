20ca0fd07cecb8e322525961cfbe4afcd4e5c2dd
viz-nu <vishnu.teja101.vt@gmail.com>
2024-05-22 23:49:29 +0530

    test: jenkins

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'build')));
 
-
+// misc
 
 startCronJob();
 dbConnect();
