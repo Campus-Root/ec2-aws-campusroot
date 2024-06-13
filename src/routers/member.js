7d689caec1540e71023d64118ca9dc7931c53491
viz-nu <vishnu.teja101.vt@gmail.com>
2024-06-13 11:34:18 +0530

    feat: graph ql test for team member route

--- src/routers/member.js
+++ src/routers/member.js
@@ -17,3 +17,83 @@ router.get("/google/login", googleAuthentication)
 router.get("/events", authMiddleware, isTeam, calendarEvents)
 
 export default router;
+
+
+
+
+
+// Import dependencies
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+import { studentModel } from "../models/Student.js";
+import applicationModel from "../models/application.js";
+import leadsModel from "../models/leads.js";
+
+// Define the GraphQL schema
+const schema = buildSchema(`
+  type Student {
+    firstName: String
+    lastName: String
+    email: String
+    displayPicSrc: String
+    phone: String
+    verification: Boolean
+    recommendations: [String]
+    preference: String
+    stage: String
+  }
+
+  type Application {
+    course: String
+    university: String
+    intake: String
+    deadline: String
+    user: String
+    approval: String
+    stage: String
+    status: String
+    cancellationRequest: String
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Lead {
+    name: String
+    email: String
+    phone: String
+    queryDescription: String
+    ifPhoneIsSameAsWhatsapp: Boolean
+    whatsappNumber: String
+    student: String
+    leadSource: String
+    leadRating: String
+  }
+
+  type Query {
+    students(page: Int!, perPage: Int!): [Student]
+    applications(page: Int!, perPage: Int!): [Application]
+    leads(page: Int!, perPage: Int!): [Lead]
+  }
+`);
+
+// Define the GraphQL root resolver
+const root = {
+    students: async ({ page, perPage }) => {
+        const skip = (page - 1) * perPage;
+        const listOfStudents = await studentModel.find().skip(skip).limit(perPage);
+        return listOfStudents;
+    },
+    applications: async ({ page, perPage }) => {
+        const skip = (page - 1) * perPage;
+        const applications = await applicationModel.find().skip(skip).limit(perPage);
+        return applications;
+    },
+    leads: async ({ page, perPage }) => {
+        const skip = (page - 1) * perPage;
+        const leads = await leadsModel.find().skip(skip).limit(perPage);
+        return leads;
+    },
+};
+
+// Set up the GraphQL endpoint
+router.use('/graphql', graphqlHTTP({ schema: schema, rootValue: root, graphiql: true,}));
