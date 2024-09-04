20a5e2157381043406e047c8194ff2debfac7ed5
viz-nu <vishnu.teja101.vt@gmail.com>
2024-09-05 00:14:21 +0530

    fix:oauth redirect

--- src/utils/oAuthClient.js
+++ src/utils/oAuthClient.js
@@ -1,4 +1,4 @@
 import 'dotenv/config';
 import { google } from 'googleapis';
 const { WEB_CLIENT_ID, WEB_CLIENT_SECRET, MEMBER_URL } = process.env;
-export const oauth2Client = new google.auth.OAuth2(WEB_CLIENT_ID, WEB_CLIENT_SECRET, `${MEMBER_URL}team/calendar-events`);
+export const oauth2Client = new google.auth.OAuth2(WEB_CLIENT_ID, WEB_CLIENT_SECRET, `${MEMBER_URL}calendar-events`);
