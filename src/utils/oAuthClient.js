da8ddd99e366f278e9988c615a4db48c64039347
viz-nu <vishnu.teja101.vt@gmail.com>
2024-09-04 21:48:46 +0530

    fix: oauth team

--- src/utils/oAuthClient.js
+++ src/utils/oAuthClient.js
@@ -1,4 +1,4 @@
 import 'dotenv/config';
 import { google } from 'googleapis';
-const { WEB_CLIENT_ID, WEB_CLIENT_SECRET, STUDENT_URL } = process.env
-export const oauth2Client = new google.auth.OAuth2(WEB_CLIENT_ID, WEB_CLIENT_SECRET, `${STUDENT_URL}team/calendar-events`);
+const { WEB_CLIENT_ID, WEB_CLIENT_SECRET, MEMBER_URL } = process.env;
+export const oauth2Client = new google.auth.OAuth2(WEB_CLIENT_ID, WEB_CLIENT_SECRET, `${MEMBER_URL}team/calendar-events`);
