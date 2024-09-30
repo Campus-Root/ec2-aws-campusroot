e13f5e5281f07e036158dd5d596188d7197988ff
viz-nu <vishnu.teja101.vt@gmail.com>
2024-09-30 14:58:13 +0530

    test: otp via mobile

--- src/utils/sendSMS.js
+++ src/utils/sendSMS.js
@@ -20,16 +20,17 @@ export const sendOTP = async (data) => {
         const { to, otp, region } = data
         switch (region) {
             case "Indian":
-                // const uniRestResponse = await unirest.post("https://www.fast2sms.com/dev/bulkV2")
-                //     .headers({ "authorization": SMS_FAST2SMS_API_KEY })
-                //     .form({ "variables_values": `${otp}`, "route": "otp", "numbers": `${to}` });
-                // return uniRestResponse.body
+            // const uniRestResponse = await unirest.post("https://www.fast2sms.com/dev/bulkV2")
+            //     .headers({ "authorization": SMS_FAST2SMS_API_KEY })
+            //     .form({ "variables_values": `${otp}`, "route": "otp", "numbers": `${to}` });
+            // return uniRestResponse.body
             case "International":
                 const accountSid = SMS_TWILIO_SID;
                 const authToken = SMS_TWILIO_TOKEN;
                 const client = new twilio(accountSid, authToken);
-                await client.messages.create({ body: `Dear customer, use this as One Time Password ${otp}. This OTP will be valid for the next 5 mins.`, from: SMS_TWILIO_NUMBER, to: to })
-                return { return: true, message: ["otp sent"], data: null }
+                const resp = await client.messages.create({ body: `Dear customer, use this as One Time Password ${otp}. This OTP will be valid for the next 5 mins.`, from: SMS_TWILIO_NUMBER, to: to })
+                console.log(resp);
+                return { return: true, message: ["otp sent"], data: resp }
             default:
                 return { return: false, message: "otp not sent as region is invalid", data: null }
         }
