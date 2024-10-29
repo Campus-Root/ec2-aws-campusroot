3aea5420adbfdf9fe6ddef7097db7ac63a9d65c6
viz-nu <vishnu.teja101.vt@gmail.com>
2024-10-30 01:37:31 +0530

    test : webhook form blogs

--- src/controllers/webhooks/index.js
+++ src/controllers/webhooks/index.js
@@ -1,3 +1,8 @@
+import { errorWrapper } from "../../middleware/errorWrapper.js";
+
 export const paymentStatus = errorWrapper(async (req, res, next, session) => {       // this is to verify payments
 
+})
+export const blogs = errorWrapper(async (req, res) => {
+    return { statusCode: 200, message: `the blog that you've sent`, data: req.body };
 })
--- src/webhooks/index.js
+++ src/webhooks/index.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import zohoFormsRouter from "./zohoForms.js"
+import { blogs } from '../controllers/webhooks/index.js';
 const router = Router();
 
 router.use("/zoho-forms", zohoFormsRouter);
-
+router.post('/blog', blogs)
 
 export default router;
