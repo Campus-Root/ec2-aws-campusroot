12b59b298f2767c95b90ab7f5840b7814ab2cc2a
viz-nu <vishnu.teja101.vt@gmail.com>
2024-11-16 15:37:30 +0530

    ai chat bot

--- src/controllers/chat/assistantReply.js
+++ src/controllers/chat/assistantReply.js
@@ -1,10 +1,11 @@
 import { errorWrapper } from "../../middleware/errorWrapper.js";
 // import { openai } from "../../utils/dbConnection.js";
 import { searchAssistant } from "../../utils/openAiEmbedding.js";
-
+import chatModel from "../../models/Chat.js"
+import userModel from "../../models/User.js"
+import messageModel from "../../models/Message.js";
 export const assistantReply = errorWrapper(async (req, res, next, session) => {
-    const { content } = req.body;
-
+    const { content, chatId } = req.body;
     // const response= await openai.chat.completions.create({
     //     model: "gpt-3.5-turbo",
     //     messages: [{
@@ -13,6 +14,19 @@ export const assistantReply = errorWrapper(async (req, res, next, session) => {
     //                 answer the following questions concisely:  "${content}"`
     //     }],
     // });
+    const chat = await chatModel.findById(chatId)
     let reply = await searchAssistant(content)
-    return { statusCode: 200, message: `reply from virtual assistant`, data: { reply: reply } }
+    let message = await messageModel.create({ sender: "6737304feb3f12f7ec92ec41", content: reply, chat: chatId })
+    chat.lastMessage = message._id
+    chat.unSeenMessages.push({ message: message._id, seen: ["6737304feb3f12f7ec92ec41"] })
+    await chat.save()
+    await userModel.populate(message, { path: "sender", select: "firstName lastName displayPicSrc email userType role" })
+    await messageModel.populate(chat, [
+        { path: "lastMessage" },
+        { path: "unSeenMessages.message", populate: { path: "sender", select: "firstName lastName displayPicSrc email userType role" } },
+    ])
+    await userModel.populate(chat, [
+        { path: "participants", select: "firstName lastName displayPicSrc email userType role" },
+    ])
+    return ({ statusCode: 200, message: `reply from virtual assistant`, data: { message, chat } })
 })
