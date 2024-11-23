649cb82bbcfedc3b8cfb82702157b911f69390f6
viz-nu <vishnu.teja101.vt@gmail.com>
2024-11-23 18:27:21 +0530

    patch: multiple locations

--- src/controllers/public/index.js
+++ src/controllers/public/index.js
@@ -41,6 +41,7 @@ export const filters = errorWrapper(async (req, res, next) => {
             break;
         case "courses":
             filter.university = { $exists: true }
+            filter.multipleLocations = { $exists: true }
             filterData.forEach(ele => {
                 if (ele.type === "country") {
                     filter["location.country"] = { $in: ele.data };
@@ -184,6 +185,7 @@ export const listings = errorWrapper(async (req, res, next, session) => {
             return ({ statusCode: 200, message: `list of all universities`, data: { list: listOfUniversities, currentPage: page, totalPages: totalPages, totalItems: totalDocs } })
         case "courses":
             filter.university = { $exists: true }
+            filter.multipleLocations = { $exists: true }
             req.body.filterData.forEach(ele => {
                 if (ele.type === "country") filter["location.country"] = { $in: ele.data };
                 else if (ele.type === "city") filter["location.city"] = { $in: ele.data };
