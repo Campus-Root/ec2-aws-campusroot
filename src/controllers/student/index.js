f074b8ae6bea0e9b47d3f7c099303779ecce5a2c
viz-nu <vishnu.teja101.vt@gmail.com>
2025-08-18 19:05:24 +0530

    sjdv

--- src/controllers/student/index.js
+++ src/controllers/student/index.js
@@ -94,7 +94,7 @@ export const generateRecommendations = errorWrapper(async (req, res, next, sessi
   const { filter, projections } = constructFilters(filterData, testScores, [...counsellorRecommendedPrograms, ...notInterestedPrograms]);
   let pipeline = [{ $match: filter }, { $project: projections }]
   const programs = await courseModel.aggregate(pipeline);
-  console.log("Programs found in genarate recommendations:", programs.length, JSON.stringify(programs, null, 2));
+  console.log("pipeline", programs.length, JSON.stringify(pipeline, null, 2));
   let recommendations = categorizePrograms(testScores, programs);
   req.user.recommendations.criteria = criteria
   req.user.recommendations.data = [...notInterestedPrograms, ...counsellorRecommendedPrograms, ...recommendations]
