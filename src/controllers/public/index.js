a8fbc8c4b50d93d4ac0ef5cd41c94532debe1016
viz-nu <vishnu.teja101.vt@gmail.com>
2025-12-24 19:13:05 +0530

    asdkvj

--- src/controllers/public/index.js
+++ src/controllers/public/index.js
@@ -450,7 +450,7 @@ export const listings = errorWrapper(async (req, res, next, session) => {
                                 multipleLocations: { $exists: false },
                                 ...filter
                             }, "name university discipline subDiscipline studyLevel applicationDetails tuitionFee.tuitionFeeType tuitionFee.tuitionFee startDate schoolName duration courseType studyMode currency stemDetails.stem AdmissionsRequirements.AcademicRequirements featured globalRankingPosition AdmissionsRequirements.LanguageRequirements loanDetails budget")
-                            // .sort({ globalRankingPosition: 1, _id: 1 })
+                            .sort({ globalRankingPosition: 1, _id: 1 })
                             .skip(skip)
                             .limit(perPage)
                             .populate("university", "name location logoSrc type uni_rating rank geoCoordinates")
