3cec6cdf0ea040f33a2ab6d994070045c363a995
viz-nu <vishnu.teja101.vt@gmail.com>
2026-03-11 00:50:42 +0530

    asvsdav

--- src/index.js
+++ src/index.js
@@ -160,7 +160,12 @@ app.use(
 
 
 
-
+app.use((req,res,next) => {
+    console.log("Host:", req.headers["host"])
+    console.log("Origin:", req.headers["origin"])
+    console.log("Referer:", req.headers["referer"])
+    next();
+});
 app.use("/api/v1", indexRouter);
 app.get("/api/test", (req, res) => {
     // referer
